a20e70251b043728119b390fdfcefafce69f3f05
Rocco Hundertmark <rocco.hundertmark@softwareone.com>
2022-02-16 14:52:05 +0100

    Updates

---
+++ precache-manifest.f7f8ac8e7cf318132bb56a59cb671e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd71255610f36b464e5334573ec2880",
+    "url": "/1337-5P34Ker/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "90f8e779f0aa453cdced",
+    "url": "/1337-5P34Ker/monsters-rolodex/static/css/main.62d87302.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/1337-5P34Ker/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1337-5P34Ker/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f8e779f0aa453cdced",
+    "url": "/1337-5P34Ker/monsters-rolodex/static/js/main.b2da788f.chunk.js"
+  },
+  {
+    "revision": "5ac0e8ad5551f3d30af1",
+    "url": "/1337-5P34Ker/monsters-rolodex/static/js/runtime-main.9a2ab2c7.js"
+  }
+]);
