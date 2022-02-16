d039dda1005b1bab0cccca7101beced21e0ad70b
Rocco Hundertmark <rocco.hundertmark@softwareone.com>
2022-02-16 14:59:37 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/1337-5P34Ker/monsters-rolodex/precache-manifest.f7f8ac8e7cf318132bb56a59cb671e25.js"
+  "/monsters-rolodex/precache-manifest.0215993d14b91b2ec73f82100204d330.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/1337-5P34Ker/monsters-rolodex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
